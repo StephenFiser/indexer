655284e3f8933ca8dfdd3cfcf78f016746b75704
Stephen Fiser <stephen@bluebear.io>
2017-05-11 18:41:12 -0400

    adds methods for finding h2 and h3 tags

--- app/core/indexer.rb
+++ app/core/indexer.rb
@@ -10,4 +10,12 @@ class Indexer
     @h1_tags ||= document.css('h1').map(&:content)
   end
 
+  def h2_tags
+    @h2_tags ||= document.css('h2').map(&:content)
+  end
+
+  def h3_tags
+    @h2_tags ||= document.css('h3').map(&:content)
+  end
+
 end
--- spec/core/indexer_spec.rb
+++ spec/core/indexer_spec.rb
@@ -16,4 +16,30 @@ describe Indexer do
     expect(indexed.h1_tags).to include("Arctic summit: Alaskan fears amid the vanishing ice")
   end
 
+  it 'finds all of the <h2> in an HTML doc' do
+    file = File.read('./spec/core/mashable_article.html')
+    indexed = Indexer.new(file)
+    expect(indexed.h2_tags).to include("Entertainment")
+    expect(indexed.h2_tags).to include("WATCH: 10 movies that inspire serious costume envy")
+
+
+    file = File.read('./spec/core/bbc_article.html')
+    indexed = Indexer.new(file)
+    expect(indexed.h2_tags).to include("News navigation")
+    expect(indexed.h2_tags).to include("'Longer summers'")
+  end
+
+  it 'finds all of the <h3> in an HTML doc' do
+    file = File.read('./spec/core/mashable_article.html')
+    indexed = Indexer.new(file)
+    expect(indexed.h3_tags).to eq([])
+
+    file = File.read('./spec/core/bbc_article.html')
+    indexed = Indexer.new(file)
+    expect(indexed.h3_tags).to include("Alaskan fears amid the vanishing ice")
+    expect(indexed.h3_tags).to include("Trump voters' verdict on Comey firing")
+  end
+
+
+
 end
