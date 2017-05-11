207ff63e959b65f2f8ff9133c6c5afc296d7b645
Stephen Fiser <stephen@bluebear.io>
2017-05-11 19:04:16 -0400

    adds links to indexer

--- app/core/indexer.rb
+++ app/core/indexer.rb
@@ -6,16 +6,22 @@ class Indexer
     @document = Nokogiri::HTML(html)
   end
 
-  def h1_tags
-    @h1_tags ||= find_content('h1')
+  def h1_tag_contents
+    @h1_tag_contents ||= find_content('h1')
   end
 
-  def h2_tags
-    @h2_tags ||= find_content('h2')
+  def h2_tag_contents
+    @h2_tag_contents ||= find_content('h2')
   end
 
-  def h3_tags
-    @h3_tags ||= find_content('h3')
+  def h3_tag_contents
+    @h3_tag_contents ||= find_content('h3')
+  end
+
+  def links
+    @links ||= document.css('a').map do |link|
+      { content: link.content, href: link.attr('href') }
+    end
   end
 
   private
--- spec/core/indexer_spec.rb
+++ spec/core/indexer_spec.rb
@@ -8,38 +8,48 @@ describe Indexer do
   it 'finds all of the <h1> in an HTML doc' do
     file = File.read('./spec/core/mashable_article.html')
     indexed = Indexer.new(file)
-    expect(indexed.h1_tags).to include("Mashable")
-    expect(indexed.h1_tags).to include("'King Arthur: Legend of the Sword' is two movies in one, and one is way better than the other")
+    expect(indexed.h1_tag_contents).to include("Mashable")
+    expect(indexed.h1_tag_contents).to include("'King Arthur: Legend of the Sword' is two movies in one, and one is way better than the other")
 
     file = File.read('./spec/core/bbc_article.html')
     indexed = Indexer.new(file)
-    expect(indexed.h1_tags).to include("Arctic summit: Alaskan fears amid the vanishing ice")
+    expect(indexed.h1_tag_contents).to include("Arctic summit: Alaskan fears amid the vanishing ice")
   end
 
   it 'finds all of the <h2> in an HTML doc' do
     file = File.read('./spec/core/mashable_article.html')
     indexed = Indexer.new(file)
-    expect(indexed.h2_tags).to include("Entertainment")
-    expect(indexed.h2_tags).to include("WATCH: 10 movies that inspire serious costume envy")
+    expect(indexed.h2_tag_contents).to include("Entertainment")
+    expect(indexed.h2_tag_contents).to include("WATCH: 10 movies that inspire serious costume envy")
 
 
     file = File.read('./spec/core/bbc_article.html')
     indexed = Indexer.new(file)
-    expect(indexed.h2_tags).to include("News navigation")
-    expect(indexed.h2_tags).to include("'Longer summers'")
+    expect(indexed.h2_tag_contents).to include("News navigation")
+    expect(indexed.h2_tag_contents).to include("'Longer summers'")
   end
 
   it 'finds all of the <h3> in an HTML doc' do
     file = File.read('./spec/core/mashable_article.html')
     indexed = Indexer.new(file)
-    expect(indexed.h3_tags).to eq([])
+    expect(indexed.h3_tag_contents).to eq([])
 
     file = File.read('./spec/core/bbc_article.html')
     indexed = Indexer.new(file)
-    expect(indexed.h3_tags).to include("Alaskan fears amid the vanishing ice")
-    expect(indexed.h3_tags).to include("Trump voters' verdict on Comey firing")
+    expect(indexed.h3_tag_contents).to include("Alaskan fears amid the vanishing ice")
+    expect(indexed.h3_tag_contents).to include("Trump voters' verdict on Comey firing")
   end
 
+  it 'finds all of the links in an HTML doc' do
+    file = File.read('./spec/core/mashable_article.html')
+    indexed = Indexer.new(file)
+    expect(indexed.links).to include({:content=>"Click Here to find out more.", :href=>"/cookie-policy/"})
+    expect(indexed.links.count).to eq(85)
 
+    file = File.read('./spec/core/bbc_article.html')
+    indexed = Indexer.new(file)
+    expect(indexed.links).to include({:content=>"Accessibility Help", :href=>"/accessibility/"})
+    expect(indexed.links.count).to eq(203)
+  end
 
 end
