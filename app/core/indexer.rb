682e3e93a19be66d0b88a161f638767c1c91374d
Stephen Fiser <stephen@bluebear.io>
2017-05-11 18:49:56 -0400

    refactors indexer; fixes typo

--- app/core/indexer.rb
+++ app/core/indexer.rb
@@ -7,15 +7,21 @@ class Indexer
   end
 
   def h1_tags
-    @h1_tags ||= document.css('h1').map(&:content)
+    @h1_tags ||= find_content('h1')
   end
 
   def h2_tags
-    @h2_tags ||= document.css('h2').map(&:content)
+    @h2_tags ||= find_content('h2')
   end
 
   def h3_tags
-    @h2_tags ||= document.css('h3').map(&:content)
+    @h3_tags ||= find_content('h3')
+  end
+
+  private
+
+  def find_content(tag)
+    document.css(tag).map(&:content)
   end
 
 end
